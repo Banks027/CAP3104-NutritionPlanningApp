3c61ed9012eced5345331287a0f5ed0801991faf
Z1Dog <ramonisaac024@gmail.com>
2026-06-30 20:27:20 -0400

    First 4 Uploads
    This is the first 4 JS where
    Config has the global variables
    Auth is login sign up and log out etc
    cookies is self explanatory
    and nav is the navigation of website

---
+++ config.js
@@ -0,0 +1,10 @@
+// The Config of the website aka
+// ALL THE SHARED SETTINGS AND GLOBAL STATES
+
+const urlBase = 'https://lampsxyz.online/LAMPAPI';
+const extension = 'php';
+
+let userId = 0;
+let FirstName = "";
+let LastName = "";
+let currentContact = null; //temp contact
---
+++ nav.js
@@ -0,0 +1,19 @@
+// Simply navigating the page
+
+function foodMenu() {
+    // was colorsMenu() -> now points at the food page instead of color.html
+    window.location.href = "food.html";
+}
+ 
+function contactsMenu() {
+    window.location.href = "contacts.html";
+}
+ 
+function goHome() {
+    window.location.href = "index.html";
+}
+ 
+function goBack() {
+    window.location.href = "menu.html";
+}
+ 
