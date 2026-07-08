487af6f38779f64bfd4d13cf52dd5a27ef59d631
Banks027 <Banks02753@gmail.com>
2026-07-08 14:41:56 -0400

    Add files via upload

--- js/survey.js
+++ js/survey.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 setupConfig();
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -60,33 +59,3 @@ function addSurvey(event) {
         signupButton.style.display = 'inline-block';
     }
 }
-=======
-const surveyForm = document.getElementById('surveyForm');
-const surveyFormWrapper = document.getElementById('surveyFormWrapper');
-const surveyResult = document.getElementById('surveyResult');
-const surveyResultText = document.getElementById('surveyResultText');
-const noneRestriction = document.getElementById('noneRestriction');
-
-// If "None" is checked under restrictions, uncheck the other restriction boxes (and vice versa)
-const restrictionCheckboxes = document.querySelectorAll('input[name="restrictions"]');
-restrictionCheckboxes.forEach(box => {
-  box.addEventListener('change', () => {
-    if (box === noneRestriction && box.checked) {
-      restrictionCheckboxes.forEach(other => {
-        if (other !== noneRestriction) other.checked = false;
-      });
-    } else if (box !== noneRestriction && box.checked) {
-      noneRestriction.checked = false;
-    }
-  });
-});
-
-surveyForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  surveyResultText.textContent = "Thanks for taking the time to fill out our survey!";
-
-  surveyFormWrapper.style.display = 'none';
-  surveyResult.style.display = 'block';
-});
->>>>>>> 60be9936a1cf09e6ab6337468e0fbd61ee74d138
